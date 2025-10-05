d6dcaf3c3e12ce91604bf51453462d8b5d17c2af
Vishnuvarthan14 <139092681+Vishnuvarthan14@users.noreply.github.com>
2025-10-05 14:41:33 +0530

    changed google and port

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 
 # Mail configuration
 app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 465))
 app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == 'True'
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
